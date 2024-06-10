e17cc875f6505090f83a398317d4d9fc4b6196fd
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-06-10 15:36:10 +0900

    타임어택

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { addNumber, minusNumber } from "./redux/slices/calculatorSlice";
+import { useState } from "react";
 
 // TODO: 계산결과를 redux를 이용한 전역상태로 관리해 보세요.
 function App() {
+  const result = useSelector((state) => state.calculator);
+  console.log(result);
+  const [value, setValue] = useState("");
+
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input onChange={(e) => setValue(e.target.value)} value={value} />{" "}
+        만큼을 <button onClick={dispatch(addNumber(value))}>더할게요</button>
+        <button onClick={dispatch(minusNumber(value))}>뺄게요</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{result}</p>
       </div>
     </div>
   );
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,1 +1,10 @@
 // TODO: store 셋업하고 slice에서 import한 reducer를 할당하세요.
+
+import { configureStore } from "@reduxjs/toolkit";
+import calculatorSlice from "../slices/calculatorSlice";
+
+const store = configureStore({
+  reducer: calculatorSlice,
+});
+
+export default store;
