6e16c5512de70bf59542ac456677c8b65a3322ed
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-06-10 15:43:35 +0900

    타임어택

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,9 @@ function App() {
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
         <input onChange={(e) => setValue(e.target.value)} value={value} />{" "}
-        만큼을 <button onClick={dispatch(addNumber(value))}>더할게요</button>
-        <button onClick={dispatch(minusNumber(value))}>뺄게요</button>
+        만큼을{" "}
+        <button onClick={() => dispatch(addNumber(value))}>더할게요</button>
+        <button onClick={() => dispatch(minusNumber(value))}>뺄게요</button>
       </div>
       <hr />
       <div>
