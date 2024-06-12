0f1658b3d6d5b75895a5ad999044b625c30137f3
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-06-12 15:17:49 +0900

    강의 내용 추가

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,21 @@
 import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
+import { useMemo } from "react";
 
 export default function TodoList({ isDone }) {
   const todos = useSelector((state) => state.todo);
   console.log(todos);
+
+  const filteredTodos = useMemo(
+    todos.filter((todo) => todo.isDone === isDone),
+    [todos]
+  );
+
   return (
     <section>
       <h2>{isDone ? "Done" : "Working..."}</h2>
       <ul>
-        {todos.map((todo) =>
+        {filteredTodos.map((todo) =>
           todo.isDone === isDone ? <TodoItem key={todo} todo={todo} /> : <></>
         )}
       </ul>
